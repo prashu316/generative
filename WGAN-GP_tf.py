1724d036b935c0e8384ac9dc815706c719978381
prashu316 <prashanth.panther@gmail.com>
2022-03-13 20:58:12 +0530

    created discriminator

--- WGAN-GP_tf.py
+++ WGAN-GP_tf.py
@@ -18,3 +18,86 @@ print(f"Shape of the images in the dataset: {train_images.shape[1:]}")
 # Reshape each sample to (28, 28, 1) and normalize the pixel values in the [-1, 1] range
 train_images = train_images.reshape(train_images.shape[0], *IMG_SHAPE).astype("float32")
 train_images = (train_images - 127.5) / 127.5
+
+def conv_block(
+    x,
+    filters,
+    activation,
+    kernel_size=(3, 3),
+    strides=(1, 1),
+    padding="same",
+    use_bias=True,
+    use_bn=False,
+    use_dropout=False,
+    drop_value=0.5,
+):
+    x = layers.Conv2D(
+        filters, kernel_size, strides=strides, padding=padding, use_bias=use_bias
+    )(x)
+    if use_bn:
+        x = layers.BatchNormalization()(x)
+    x = activation(x)
+    if use_dropout:
+        x = layers.Dropout(drop_value)(x)
+    return x
+
+
+def get_discriminator_model():
+    img_input = layers.Input(shape=IMG_SHAPE)
+    # Zero pad the input to make the input images size to (32, 32, 1).
+    x = layers.ZeroPadding2D((2, 2))(img_input)
+    x = conv_block(
+        x,
+        64,
+        kernel_size=(5, 5),
+        strides=(2, 2),
+        use_bn=False,
+        use_bias=True,
+        activation=layers.LeakyReLU(0.2),
+        use_dropout=False,
+        drop_value=0.3,
+    )
+    x = conv_block(
+        x,
+        128,
+        kernel_size=(5, 5),
+        strides=(2, 2),
+        use_bn=False,
+        activation=layers.LeakyReLU(0.2),
+        use_bias=True,
+        use_dropout=True,
+        drop_value=0.3,
+    )
+    x = conv_block(
+        x,
+        256,
+        kernel_size=(5, 5),
+        strides=(2, 2),
+        use_bn=False,
+        activation=layers.LeakyReLU(0.2),
+        use_bias=True,
+        use_dropout=True,
+        drop_value=0.3,
+    )
+    x = conv_block(
+        x,
+        512,
+        kernel_size=(5, 5),
+        strides=(2, 2),
+        use_bn=False,
+        activation=layers.LeakyReLU(0.2),
+        use_bias=True,
+        use_dropout=False,
+        drop_value=0.3,
+    )
+
+    x = layers.Flatten()(x)
+    x = layers.Dropout(0.2)(x)
+    x = layers.Dense(1)(x)
+
+    d_model = keras.models.Model(img_input, x, name="discriminator")
+    return d_model
+
+
+d_model = get_discriminator_model()
+d_model.summary()
